821660f2fcd1ebd0aecd5dcb2c3c323243a14d54
ASUS <ASUS@LAPTOP-JOBMVT2Q>
2019-03-17 23:09:42 +0530

    drag and drop action commit

---
+++ automateTest/src/actionsClass/dragAndDrop.java
@@ -0,0 +1,68 @@
+package actionsClass;
+
+import static org.junit.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class dragAndDrop {
+
+	WebDriver driver;
+	String baseUrl;
+
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		baseUrl = "https://jqueryui.com/droppable/";
+	
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void test() throws InterruptedException {
+		driver.get(baseUrl);
+		Thread.sleep(3000);
+		driver.switchTo().frame(0);
+		
+		WebElement fromElement = driver.findElement(By.id("draggable"));
+		WebElement toElement = driver.findElement(By.id("droppable"));
+		
+		Actions action = new Actions(driver);
+		
+		//Drag and drop
+	     action.dragAndDrop(fromElement, toElement).build().perform();
+         Thread.sleep(2000);
+	}
+	
+	@Test
+	public void test2() throws InterruptedException {
+		driver.get(baseUrl);
+		Thread.sleep(3000);
+		driver.switchTo().frame(0);
+		
+		WebElement fromElement = driver.findElement(By.id("draggable"));
+		WebElement toElement = driver.findElement(By.id("droppable"));
+		
+		Actions action = new Actions(driver);
+		
+		//Click and hold, move to element
+		action.clickAndHold(fromElement).moveToElement(toElement).release().build().perform();
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		Thread.sleep(2000);
+		driver.quit();
+	}
+}
