3b336d1ec5d7b806ef5c18162e38f7862870d7fb
ASUS <ASUS@LAPTOP-JOBMVT2Q>
2019-04-05 11:14:08 +0530

    testNG priority commit

--- TestNG/src/testClasses/TestNG_Priority.java
+++ TestNG/src/testClasses/TestNG_Priority.java
@@ -15,20 +15,20 @@ public class TestNG_Priority {
 	public void cleanUp() {
 		System.out.println("\nTestNG_Priority ->  after class");
 	}
-	
-	@Test(priority=2)
+
+	@Test(priority = 2)
 	public void testMethod1() {
 		System.out.println("\nTestNG_Priority ->  testMethod1");
 	}
-	
-	@Test(priority=1)
+
+	@Test(priority = 1)
 	public void testMethod2() {
-		System.out.println("\nTestNG_Priority ->  testMethod2");	
+		System.out.println("\nTestNG_Priority ->  testMethod2");
 	}
-	
-	@Test(priority=0)
+
+	@Test(priority = 0)
 	public void testMethod3() {
-		System.out.println("\nTestNG_Priority ->  testMethod3");		
+		System.out.println("\nTestNG_Priority ->  testMethod3");
 	}
 
 }
