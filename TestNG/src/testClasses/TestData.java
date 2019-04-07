f0d52dbdd73c6f869edd82337420566e7d137336
ASUS <ASUS@LAPTOP-JOBMVT2Q>
2019-04-07 08:30:40 +0530

    data providers commit

---
+++ TestNG/src/testClasses/TestData.java
@@ -0,0 +1,15 @@
+package testClasses;
+
+import org.testng.annotations.DataProvider;
+
+public class TestData {
+
+	@DataProvider(name = "inputs")
+	public Object[][] getData() {
+
+		return new Object[][] {
+
+				{ "bmw", "m3" }, { "audi", "a6" }, { "benz", "c300" } };
+	}
+
+}
